fab882922129efb9e41272c03399e28ad0d08489
parth-106 <68140481+parth-106@users.noreply.github.com>
2020-07-16 22:23:14 +0530

    Add files via upload

---
+++ Assignment-4.2/assignment4.js
@@ -0,0 +1,12 @@
+console.log("Assignment 4.2");
+
+let OS = prompt("Enter OS name");
+let VS = prompt("Enter Your Version");
+if(OS == "Android");
+if(VS == "9"){
+    OS.match("Android")
+    VS.match("9");
+    console.log(`The OS name is ${OS}. and Version is ${VS}.`);
+}else {
+    console.log("Pls Check Your Android name and OS Version Once Again")
+};
